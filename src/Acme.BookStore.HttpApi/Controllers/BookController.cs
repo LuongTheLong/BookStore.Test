e8df303cc6b709592f7f854de155014ec4b46248
ObitoTob1 <lrollking1@gmail.com>
2021-07-02 17:02:42 +0700

    Add project files.

---
+++ src/Acme.BookStore.HttpApi/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using Acme.BookStore.Books;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Acme.BookStore.Controllers
+{
+    [RemoteService]
+    [Route("/api/BookStore")]
+    public class BookController : AbpController
+    {
+        private readonly IBookAppService _bookAppService;
+        public BookController(IBookAppService bookAppService)
+        {
+            _bookAppService = bookAppService;
+        }
+
+        //Lay tat ca book 
+        [HttpGet("/Books")]
+        public async Task<IActionResult> GetAllBook()
+        {
+            var listBook = await _bookAppService.GetAllBook();
+            return Ok(listBook);
+        }
+    }
+}
---
+++ src/Acme.BookStore.HttpApi/Controllers/BookStoreController.cs
@@ -0,0 +1,20 @@
+﻿using Acme.BookStore.Books;
+using Acme.BookStore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Acme.BookStore.Controllers
+{
+    /* Inherit your controllers from this class.
+     */
+
+    public abstract class BookStoreController : AbpController
+    {
+        protected BookStoreController(IBookAppService bookAppService)
+        {
+            LocalizationResource = typeof(BookStoreResource);
+           
+        }
+    }
+}
